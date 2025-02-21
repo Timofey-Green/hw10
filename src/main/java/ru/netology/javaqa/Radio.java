28ea391235442a9ab66f8c093ecabc9f8abd7236
Timofey <t.andreev-mcn@yandex.ru>
2025-02-21 20:20:30 +0300

    add dependences

---
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -0,0 +1,90 @@
+package ru.netology.javaqa;
+
+public class Radio {
+
+    public int station;
+    public int volume;
+
+    //НАСТРОЙКА РАДИОСТАНЦИИ.
+
+    public int getStation() {
+        return station;
+    }
+
+
+    // Установили максимальные границы ввода.
+
+    public void setStation(int newStation) {
+        if (newStation < 0) {
+            return;
+        }
+        if (newStation > 9) {
+            return;
+        }
+        station = newStation;
+    }
+
+    // Установили шаги переключения станций.
+
+    public void nextStation() {
+        if (station < 9) {
+            station = station + 1;
+        } else {
+            station = 0;
+        }
+
+
+    }
+
+
+    public void prevStation() {
+        if (station > 0) {
+            station = station - 1;
+        } else {
+            station = 9;
+        }
+
+    }
+
+
+
+    //НАСТРОЙКА ГРОМКОСТИ
+
+    public int getVolume() {
+        return volume;
+    }
+
+    // Установили максимальную громкость - 100.
+
+    public void setToMaxVolume() {
+        volume = 100;
+    }
+
+    // Установили верхнюю и нижнюю границу громкости.
+
+    public void setVolume(int newVolume) {
+        if (newVolume < 0) {
+            return;
+        }
+        if (newVolume > 100) {
+            return;
+        }
+    }
+
+    // Устанавливаем шаги прибавления и убавления громкости
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume = volume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume = volume - 1;
+        }
+
+    }
+
+
+}
---
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -0,0 +1,115 @@
+package ru.netology.javaqa;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RadioTest {
+
+    // ТЕСТИРУЕМ ПЕРКЛЮЧЕНИЕ РАДИОСТАНЦИИ
+    // Проверяем ввод валидного значения, нижней границы, верхней границы
+    // и неваличных значений по двум границам.
+
+
+    @Test //Устанавливаем
+
+    public void shouldStation () {
+        Radio rad = new Radio();
+
+        rad.setStation(5);
+
+        int expected = 5;
+        int actual = rad.getStation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void theLowerLimit() {
+        Radio rad = new Radio();
+
+        rad.setStation(1);
+
+        int expected = 1;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void theUpperLimit() {
+        Radio rad = new Radio();
+
+        rad.setStation(9);
+
+        int expected = 9;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void invalidMaxValue() {
+        Radio rad = new Radio();
+
+        rad.setStation(10);
+
+        int expected = 0;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void invalidMinValue() {
+        Radio rad = new Radio();
+
+        rad.setStation(-1);
+
+        int expected = 0;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Проверяем переключение станций с 0 на 9 радиостанцию и с 9 на 0 радиостанцию.
+
+    @Test
+    public void switchingFrom0To9() {
+        Radio rad = new Radio();
+
+        rad.setStation(0);
+
+        rad.prevStation();
+
+        int expected = 9;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+
+
+    public void switchingFrom9To0 () {
+        Radio rad = new Radio();
+        rad.setStation(9);
+
+        rad.nextStation();
+
+        int expected = 0;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchingFrom4To5 () {
+        Radio rad = new Radio();
+        rad.setStation(4);
+
+        rad.nextStation();
+
+        int expected = 5;
+        int actual =rad.getStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    
+
+}
